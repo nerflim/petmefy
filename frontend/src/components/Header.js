d3fd0b5a9f63cb91ac123450d227a7e349e2147c
Milfren <nerflimjohn@gmail.com>
2019-08-01 22:35:26 +1000

    changed menu interface

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
-		<nav className='navbar flex items-center justify-between flex-wrap p-6 w-full fixed text-white container rounded-full mt-5 shadow bg-main'>
+		<nav className='navbar flex items-center justify-between flex-wrap px-6 py-2 w-full fixed text-gray-400 shadow bg-gray-700 leading-none border-t-4 border-orange-500'>
 			<div className='flex items-center flex-shrink-0 mr-6'>
 				<Link to='/' className='text-lg font-bold'>
 					PETMEFY
@@ -19,15 +19,25 @@ const Header = () => {
 			</div>
 			<div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
 				<div className='text-sm lg:flex-grow'>
-					<Link to='/shop' className='block mt-4 lg:inline-block lg:mt-0 mr-4'>
+					<Link to='/shop' className='block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-orange-500'>
 						Shop
 					</Link>
-					<Link to='/adopt' className='block mt-4 lg:inline-block lg:mt-0 mr-4'>
+					<Link to='/adopt' className='block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-orange-500'>
 						Adopt
 					</Link>
-					<a href='!#' className='block mt-4 lg:inline-block lg:mt-0 mr-'>
+					<Link to='/' className='block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-orange-500'>
 						Community
-					</a>
+					</Link>
+				</div>
+				<div>
+					<Link to='/' className='block mt-4 lg:inline-block lg:mt-0 mr-4 hover:text-orange-500'>
+						Sign in
+					</Link>
+					<Link
+						to='/'
+						className='block mt-4 lg:inline-block lg:mt-0 px-4 py-2 border rounded hover:border-orange-500 hover:bg-gray-600 hover:text-orange-500 hover:shadow'>
+						Sign up
+					</Link>
 				</div>
 			</div>
 		</nav>
