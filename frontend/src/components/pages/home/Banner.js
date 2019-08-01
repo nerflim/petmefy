21b1e3250d12cc706344fc48fae623c8c586f780
Milfren <nerflimjohn@gmail.com>
2019-08-01 23:11:42 +1000

    modified banner to save space

--- frontend/src/components/pages/home/Banner.js
+++ frontend/src/components/pages/home/Banner.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
 	return (
-		<div className='home-banner'>
-			<div className='container mx-auto'>
+		<div className='flex home-banner items-center text-gray-900'>
+			<div className='flex-1 container mx-auto'>
 				<h1 className='leading-none mb-3'>
 					Pet Community
 					<br />
 					for everyone
 				</h1>
-				<h4 className='text-xl mb-8'>Helping our pets reach out to each other and to those who need home.</h4>
-				<a href='#' className='btn btn-lg btn-outline-light mt-3 p-4 bg-main hover:bg-blue-600 text-white rounded text-xl'>
+				<h4 className='text-base mb-5'>Helping pets reach out to each other and to those who need home.</h4>
+				<Link to='/' className='border border-orange-500 btn btn-lg hover:bg-gray-700 p-3 rounded text-orange-500 text-xl inline-block'>
 					Sign Up
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
--- frontend/src/components/pages/home/FeaturedItem.js
+++ frontend/src/components/pages/home/FeaturedItem.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const FeaturedItem = () => {
 	return (
-		<div className='max-w-sm rounded overflow-hidden shadow bg-white p-2'>
+		<Link to='/' className='max-w-sm rounded overflow-hidden shadow bg-white p-2 block'>
 			<div className='relative'>
 				<span className='inline-block bg-teal-200 rounded-full px-3 py-1 text-xs text-gray-700 absolute right-0 m-2'>FOR SALE</span>
 				<img className='w-full' src='https://tailwindcss.com/img/card-top.jpg' alt='Sunset in the mountains' />
@@ -11,7 +12,7 @@ const FeaturedItem = () => {
 				<p className='font-bold text-sm'>I'm a pet</p>
 				<p className='text-xs text-gray-500'>Breed type here</p>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
